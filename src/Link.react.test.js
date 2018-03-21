3ade0321b8d68fbdff526acdb2f1045a53bb2b9c
hydraslay <xvkai2010@gmail.com>
2018-03-21 17:09:37 +0900

    move "default href" case to same file

--- src/Link.react.test.js
+++ src/Link.react.test.js
@@ -3,10 +3,29 @@ import Link from './Link.react';
 import renderer from 'react-test-renderer';
 
 test('Link changes the class when hovered', () => {
-  const component = renderer.create(
+  let component = renderer.create(
     <Link page="http://www.facebook.com">Facebook</Link>,
   );
   let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  tree.props.onMouseEnter();
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  tree.props.onMouseLeave();
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // default href prop
+  component = renderer.create(
+    <Link>Facebook</Link>,
+  );
+  tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 
   // manually trigger the callback
